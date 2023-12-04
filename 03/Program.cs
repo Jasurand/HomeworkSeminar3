725c49a891c7afd5e05edccc712c1de81d603ed5
Jasand <smileonyourface@yandex.com>
2023-12-05 02:14:59 +0500

    Third task is ready, but it has problem

---
+++ 03/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] FillingArray(int n)
+{
+    double[] arr1 = new double[n];
+    for (int i1 = 0; i1 < n; i1++) arr1[i1] = new Random().Next(1, 100);
+    return arr1;
+}
+
+void PrintingArray(double[] arr2)
+{
+    for (int i2 = 0; i2 < arr2.Length; i2++) Console.Write(arr2[i2] + " ");
+    Console.WriteLine();
+}
+
+double SearchingMin(double[] arr3)
+{
+    double min = arr3[0];
+    for (int i3 = 0; i3 < arr3.Length; i3++)
+    {
+        if (arr3[i3] < min) min = arr3[i3];
+    }
+    return min;
+}
+
+double SearchingMax(double[] arr4)
+{
+    double max = arr4[0];
+    for (int i4 = 0; i4 < arr4.Length; i4++)
+    {
+        if (arr4[i4] > max) max = arr4[i4];
+    }
+    return max;
+}
+
+double[] arr = FillingArray(10);
+PrintingArray(arr);
+double Max = SearchingMax(arr);
+double Min = SearchingMin(arr);
+Console.WriteLine($"{Max} - {Min} = {Max - Min}");
